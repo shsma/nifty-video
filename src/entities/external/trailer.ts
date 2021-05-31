467ec52aa3ead71fdeaf8d6a746da4f44f1db199
Shady Smaoui <shadysmaoui@hotmail.fr>
2021-05-31 01:15:16 -0400

    trailer module logic + previous/next

---
+++ src/entities/external/trailer.ts
@@ -0,0 +1,9 @@
+export interface Trailer {
+    id: number;
+    key: string;
+    name: string;
+    site: string;
+    size: number;
+    type: string;
+    movieId: number;
+}
--- src/store/movie.module.ts
+++ src/store/movie.module.ts
@@ -11,6 +11,7 @@ import {
 import MovieGateway from '@/api/gateways/MovieGateway';
 import { Movie } from '@/entities/external/movie';
 import SERVICE_IDENTIFIER from '@/identifiers';
+import { Trailer } from '@/entities/external/trailer';
 
 @Module({ generateMutationSetters: true })
 export default class MovieModule extends VuexModule {
@@ -24,6 +25,11 @@ export default class MovieModule extends VuexModule {
     private _page = Math.floor(Math.random() * 1000);
     private _movieList: [Movie] | null = null;
     private _movie: Movie | null = null;
+    private _trailer: Trailer | null = null;
+
+    get page(): number {
+        return this._page;
+    }
 
     get movieList(): [Movie] | null {
         return this._movieList;
@@ -33,19 +39,8 @@ export default class MovieModule extends VuexModule {
         return this._movie;
     }
 
-    get page(): number {
-        return this._page;
-    }
-
-    @Mutation
-    public setMovieList(movieList: [Movie]): void {
-        console.log(movieList);
-        this._movieList = movieList;
-    }
-
-    @Mutation
-    public setMovie(movie: Movie): void {
-        this._movie = movie;
+    get trailer(): Trailer | null {
+        return this._trailer;
     }
 
     @Mutation
@@ -58,13 +53,28 @@ export default class MovieModule extends VuexModule {
         this._page = Math.floor(Math.random() * 1000);
     }
 
+    @Mutation
+    public setMovieList(movieList: [Movie]): void {
+        this._movieList = movieList;
+    }
+
+    @Mutation
+    public setMovie(movie: Movie): void {
+        this._movie = movie;
+    }
+
+    @Mutation
+    public setTrailer(trailer: Trailer | null): void {
+        this._trailer = trailer;
+    }
+
     @Action
     public async fetchMovies(): Promise<void> {
         try {
             let response: any;
             do {
                 this.randomizePage();
-                response = await this.gateway.list(this.page);
+                response = await this.gateway.listMovies(this.page);
             } while (response.data.item_count < 30);
             this.setMovieList(response.data.items);
         } catch (error) {
@@ -73,12 +83,74 @@ export default class MovieModule extends VuexModule {
     }
 
     @Action
-    public async getMovie(id: number): Promise<void> {
+    public async fetchMovie(id: number): Promise<void> {
         try {
-            const response = await this.gateway.get(id);
+            const response = await this.gateway.fetchMovie(id);
             this.setMovie(response.data);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    @Action
+    public async fetchTrailer(id: number): Promise<void> {
+        try {
+            const response = await this.gateway.fetchTrailer(id);
+            this.setTrailer(response.data.results[0]);
+            if (this._trailer) {
+                this._trailer.movieId = id;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    @Action
+    public async nextTrailer(): Promise<void> {
+        try {
+            if (this._movieList && this._trailer) {
+                const currentIndex = this._movieList.findIndex(
+                    (movie: Movie) => movie.id === this._trailer?.movieId
+                );
+                const response = await this.gateway.fetchTrailer(
+                    this._movieList[currentIndex + 1].id
+                );
+                this.setTrailer(response.data.results[0]);
+                if (this._trailer) {
+                    this._trailer.movieId = this._movieList[
+                        currentIndex + 1
+                    ].id;
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    @Action
+    public async previousTrailer(): Promise<void> {
+        try {
+            if (this._movieList && this._trailer) {
+                const currentIndex = this._movieList.findIndex(
+                    (movie: Movie) => movie.id === this._trailer?.movieId
+                );
+                const response = await this.gateway.fetchTrailer(
+                    this._movieList[currentIndex - 1].id
+                );
+                this.setTrailer(response.data.results[0]);
+                if (this._trailer) {
+                    this._trailer.movieId = this._movieList[
+                        currentIndex - 1
+                    ].id;
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    @Action
+    public closeTrailer(): void {
+        this.setTrailer(null);
     }
 }
