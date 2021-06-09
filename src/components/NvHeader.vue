dfce238800b847c06951d49ac9a47de501a6c0fd
Khouloudlaarous <khouloudaissa@gmail.com>
2021-06-09 23:39:18 +0100

    add style

--- src/components/NvHeader.vue
+++ src/components/NvHeader.vue
@@ -1,33 +1,23 @@
 <template>
     <header class="header">
-        <div class="showcase-top">
-            <img src="../assets/logo.png" alt="header" />
-            <a class="nav-a" href="#">
-                <i class="fa fa-globe" aria-hidden="true"></i>
-                <i class="fa fa-caret-down" aria-hidden="true"></i>
-                <span>Français</span>
-            </a>
-            <a href="#" class="btn btn-rounded">Sign in</a>
-        </div>
-        <div class="showcase-content">
-            <h1>
-                Film,séries TV et bien <br />
-                plus en illimité.
-            </h1>
-            <p>Où que vous soyez. Annulez à tout moment</p>
-            <h2>
-                Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour
-                vous abonner où réactiver votre <br />abonnement.
-            </h2>
-            <form action="/action_page.php">
-                <label for="email">Enter your email:</label>
-                <input type="email" id="email" name="email" />
-                <a href="#" class="btn btn-xl">
-                    Commencer <i class="fas fa-chevron-right btn-icon"></i>
-                </a>
-            </form>
+        <div class="inner_header">
+            <div class="logo_container">
+                <h1>Nifty<span>VIDEO</span></h1>
+            </div>
+            <ul class="useful">
+                <a><li>About</li></a>
+                <a><li>Contact</li></a>
+                <a><li>Sign in</li></a>
+            </ul>
         </div>
     </header>
+    <section>
+        <div class="tag_line">
+            <h2>The best suggestion of movies to watch</h2>
+            <br />
+            <h3>Are in <span>Here</span></h3>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
@@ -40,24 +30,96 @@ export default class NvHeader extends Vue {}
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+}
 .header {
     width: 100%;
-    height: 93vh;
-    position: relative;
-    background-size: cover cover;
-    background-repeat: no-repeat;
-    background-position: center;
+    height: 80px;
+    display: block;
+    background-color: #101010;
+}
+.inner_header {
+    width: 1000px;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
 }
 
-.showcase::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
+.logo_container {
     height: 100%;
-    z-index: 1;
-    background: rgba(0, 0, 0, 0.6);
-    box-shadow: inset 120px 100px 250px #000000;
+    display: table;
+    float: left;
+}
+
+.logo_container h1 {
+    color: crimson;
+    height: 100%;
+    display: table-cell;
+    vertical-align: middle;
+    font-family: 'Montserrat';
+    font-size: 32px;
+    font-weight: 200;
+}
+
+.logo_container h1 span {
+    font-weight: 800;
+    color: white;
+}
+
+.useful {
+    float: right;
+    height: 100%;
+}
+
+.useful a {
+    height: 100%;
+    display: table;
+    float: left;
+    padding: 0 20px;
+}
+
+.useful a:last-child {
+    padding-right: 0;
+}
+
+.useful a li {
+    display: table-cell;
+    vertical-align: middle;
+    height: 100%;
+    color: white;
+    font-family: 'Montserrat';
+    font-size: 16px;
+}
+
+section {
+    background-image: ;
+}
+.tag_line h2 {
+    font-weight: 800;
+    color: crimson;
+    font-family: 'Roboto Condensed', 'Roboto', sans-serif;
+    font-style: italic;
+    padding: 20px;
+    text-transform: uppercase;
+}
+
+.tag_line h3 {
+    font-weight: 600;
+    color: crimson;
+    font-family: 'Roboto Condensed', 'Roboto', sans-serif;
+    font-style: italic;
+    text-transform: uppercase;
+}
+
+.tag_line h3 span {
+    font-weight: 600;
+    color: black;
+    font-family: 'Roboto Condensed', 'Roboto', sans-serif;
+    font-style: italic;
+    text-transform: uppercase;
 }
 </style>
